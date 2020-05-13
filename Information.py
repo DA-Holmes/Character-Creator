4dfcbe0356ea51641e7122590b26711b00251b3f
David <you@example.com>
2020-05-12 21:51:35 -0500

    David Update 5.12
    *includes file name customization
    *updated comments for statbuilder *added a bunch of goals in mainfile
    *expanded boolean answer cache and prompt for y/n
    *created tutorial opt-in to replace the helper function. keeping function for now just in case

--- Information.py
+++ Information.py
@@ -3,26 +3,26 @@ This class includes the optional additional information to be printed to keep th
 '''
 
 class Information:
-    
+
     def __init__(self):
         self.intro = "More information"
-        
+
     def dnd(self):
         print()
         print("Dungeons and Dragons is a tabletop roleplaying game in which you create a fictional character to")
         print("interact with the world designed by the host of the game.")
-        
+
     def race(self):
         print()
         print("There are a variety of different lands and cultures in the world of D&D, with a variety of races")
         print("to match. Each of the races comes with unique special abilities or skills, as shown below.")
-        
+
     def dnd_class(self):
         print()
         print("Classes are essentially the job or role of your character. These influence a character's skills")
         print("and the methods they may use to interact with the world, wether it be through brute force or")
         print("more subtle tactics.")
-    
+
     def stats(self):
         print()
         print("Stats are your character's proficiencies with different skills, such as strength or charisma. You")
